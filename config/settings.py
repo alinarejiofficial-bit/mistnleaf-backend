c46cf0fb87d1bc5034a6c0a07d470a567e92b118
alina reji <alinarejiofficial@gmail.com>
2026-09-10 15:31:51 +0530

    Update ALLOWED_HOSTS in settings.py to include production and local environments for enhanced deployment flexibility.

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
+ALLOWED_HOSTS = [
+    "mistnleaf-backend.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 DJANGO_APPS = [
